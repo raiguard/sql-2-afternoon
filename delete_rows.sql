4ea2a3616de337a15cceb2b760c3a5f907bb83ca
raiguard <3515394+raiguard@users.noreply.github.com>
2020-07-09 15:52:22 -0600

    Create delete_rows.sql

---
+++ delete_rows.sql
@@ -0,0 +1,28 @@
+-- 1
+-- CREATE TABLE practice_delete ( name TEXT, type TEXT, value INTEGER );
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'bronze', 50);
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'bronze', 50);
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'bronze', 50);
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'silver', 100);
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'silver', 100);
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'gold', 150);
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'gold', 150);
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'gold', 150);
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'gold', 150);
+
+-- SELECT * FROM practice_delete;
+
+-- 2
+-- delete
+-- from practice_delete
+-- where type = 'bronze';
+
+-- 3
+-- delete
+-- from practice_delete
+-- where type = 'silver';
+
+-- 4
+-- delete
+-- from practice_delete
+-- where value = 150;
